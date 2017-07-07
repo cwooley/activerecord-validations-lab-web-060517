efd19ef1c949e735e06c0b61d429e61048eb28cc
github username <github email address>
2017-07-07 14:03:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :summary, length: {in: (0..250)}
+  validates :content, length: {minimum: 250}
+  validates :category, inclusion: ['Fiction', 'Non-fiction']
+  validate :clickbait
+  def clickbait
+    if self.title
+      unless self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Guess")
+        errors.add(:Category, 'category must be either Fiction or Non-fiction')
+      end
+    end
+  end
 end
